e75668246cb46429794c1283263002228acbd8b3
giladk123 <giladk@gmail.com>
2024-07-06 23:29:03 +0300

    test

--- examples/complete/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource-groups" {
-  value = module.spoke.resource-groups
-  description = "value of resource-groups"
-}
-
-output "vnets" {
-  value = module.spoke.vnets
-  description = "value of vnets"
-}
